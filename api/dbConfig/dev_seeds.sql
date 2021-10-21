b9867ac6b3a208af5a381e10db5508ea37ee8706
gretaivan <greta.ivan91@gmail.com>
2021-10-21 16:07:41 +0100

    adds more comments to explain the code

---
+++ api/dbConfig/dev_seeds.sql
@@ -0,0 +1,1 @@
+-- seeds in some data for development purposes 
--- api/server.js
+++ api/server.js
@@ -1,7 +1,14 @@
+/**
+ * Server file that defines the server connection
+ * cors is used to authorise a certain IP range i.e. all connection or limit
+ * also in this case it defines api routes too, if functionality would require more routes
+ * we would use another file/module to abstract them according to existing data models and then controller file to define route for each abstracted route 
+ */
 const express = require('express')
 const cors = require('cors')
 
 const server = express()
+//no config for cors at the moment because it is not a deployed app, before deploying it would specify who we allow to use this api or allow everybody
 server.use(cors())
 
 server.get('/', (req, res) => {
